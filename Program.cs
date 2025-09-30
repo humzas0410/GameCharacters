1484603a94ce2cf7ae6ee210616743ba995f1312
Humza Syed <hsyed@my.wctc.edu>
2025-09-30 14:21:55 -0500

    [1.12b] Remove Character - first match

--- Program.cs
+++ Program.cs
@@ -53,7 +53,10 @@ do
         if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
         {
             logger.Info($"Character Id {Id} entered");
-        } else {
+            Mario? character = marios.First(c => c.Id == Id);
+        }
+        else
+        {
             logger.Error("Invalid Id");
         }
     }
